cf6ba07d69adc650a88243946c1cc5ef2ff31bd2
Juan José González Abril <SackCastellon@users.noreply.github.com>
2018-12-19 12:59:32 +0100

    Extraidas versiones de 'koin' y 'ikonli' a 'gradle.properties'

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,11 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+val koinVersion: String by project
+val ikonliVersion: String by project
+
 plugins {
     application
-    kotlin("jvm") version "1.3.10"
+    kotlin("jvm") version "1.3.11"
     id("com.github.ben-manes.versions") version "0.20.0"
 }
 
@@ -10,6 +13,7 @@ group = "es.uji.ei1039"
 version = "1.0-SNAPSHOT"
 
 repositories {
+    mavenLocal()
     mavenCentral()
     jcenter()
     maven("https://oss.sonatype.org/content/repositories/snapshots")
@@ -18,11 +22,13 @@ repositories {
 dependencies {
     // Kotlin Standard Library
     implementation(kotlin("stdlib-jdk8"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-javafx:1.0.1")
 
     // Dependency Injection
-    implementation("org.koin:koin-core:1.0.2")
-    implementation("org.koin:koin-core-ext:1.0.2")
-    implementation("org.koin:koin-logger-slf4j:1.0.2")
+    implementation("org.koin:koin-core:$koinVersion")
+    implementation("org.koin:koin-core-ext:$koinVersion")
+    implementation("org.koin:koin-logger-slf4j:$koinVersion")
+
     implementation("com.github.ben-manes.caffeine:caffeine:2.6.2")
     implementation("com.google.guava:guava:27.0.1-jre")
 
@@ -30,13 +36,19 @@ dependencies {
     implementation("no.tornado:tornadofx:1.7.18-SNAPSHOT")
     implementation("no.tornado:tornadofx-controlsfx:0.1.1")
     implementation("org.controlsfx:controlsfx:8.40.14")
+    implementation("org.jetbrains.kotlinx:kotlinx-html-jvm:0.6.11")
 
     // Icons
-    implementation("org.kordamp.ikonli:ikonli-javafx:2.4.0")
-    implementation("org.kordamp.ikonli:ikonli-fontawesome-pack:2.4.0")
+    implementation("org.kordamp.ikonli:ikonli-javafx:$ikonliVersion")
+    implementation("org.kordamp.ikonli:ikonli-fontawesome-pack:$ikonliVersion")
+
+    // Databases
+    implementation("org.jetbrains.exposed:exposed:0.11.2")
+    implementation("org.xerial:sqlite-jdbc:3.25.2")
+    implementation("org.postgresql:postgresql:42.2.5")
 
     // Validation
-    implementation("com.googlecode.libphonenumber:libphonenumber:8.10.1")
+    implementation("com.googlecode.libphonenumber:libphonenumber:8.10.2")
 
     // Logging
     implementation("ch.qos.logback:logback-classic:1.3.0-alpha4")
