472e3d6fa9cc1bfab38c646780436432d7470e96
Juan José González Abril <SackCastellon@users.noreply.github.com>
2019-01-05 23:41:53 +0100

    Arreglados bugs en procesado de 'queries'.
    Ahora el resultado del procesado 'queries' se muestra en el 'log'.

--- src/main/kotlin/agenda/util/ContactQuery.kt
+++ src/main/kotlin/agenda/util/ContactQuery.kt
@@ -5,6 +5,7 @@ import agenda.util.ContactQuery.Companion.FieldState.NAME
 import agenda.util.ContactQuery.Companion.FieldState.VALUE
 import agenda.util.ContactQuery.Companion.State.*
 import com.github.benmanes.caffeine.cache.Caffeine
+import mu.KotlinLogging
 import java.util.*
 
 sealed class ContactQuery {
@@ -14,9 +15,10 @@ sealed class ContactQuery {
     private object EmptyQuery : ContactQuery() {
         override val predicate: (Contact) -> Boolean = { true }
         override val comparator: Comparator<in Contact> = compareBy(Contact::id)
+        override fun toString(): String = this::class.simpleName ?: super.toString()
     }
 
-    private class SimpleQuery(words: List<String>) : ContactQuery() {
+    private data class SimpleQuery(private val words: Set<String>) : ContactQuery() {
         private val cache = Caffeine.newBuilder()
             .build<Contact, Int> { contact ->
                 val fieldValues =
@@ -32,18 +34,21 @@ sealed class ContactQuery {
         override val comparator: Comparator<in Contact> = compareBy({ -cache[it]!! }, Contact::id)
     }
 
-    private class ComplexQuery(words: List<String>, fields: Map<String, String>) : ContactQuery() {
+    private data class ComplexQuery(private val words: Set<String>, private val fields: Map<String, Set<String>>) : ContactQuery() {
         companion object {
             @JvmField internal val fieldMap = mapOf(
-                "firstName" to Contact::firstName,
-                "lastName" to Contact::lastName,
-                "fullName" to Contact::fullName
+                "firstName" to Contact::firstName.getter,
+                "lastName" to Contact::lastName.getter,
+                "fullName" to Contact::fullName.getter
             )
         }
 
         private val cache = Caffeine.newBuilder()
             .build<Contact, Int> { contact ->
-                val count = fields.count { (name, value) -> fieldMap.getValue(name).invoke(contact).contains(value) }
+                val count = fields.entries.sumBy { (name, values) ->
+                    val field = fieldMap.getValue(name).invoke(contact)
+                    values.count { field.contains(it, ignoreCase = true) }
+                }
                 if (count == 0) 0
                 else {
                     val fieldValues =
@@ -52,7 +57,7 @@ sealed class ContactQuery {
                                 contact.emails.flatMap { listOf(it.label.name, it.email) } +
                                 contact.groups.map { it.name }
 
-                    words.sumBy { w -> fieldValues.count { it.contains(w, true) } } + count
+                    words.sumBy { w -> fieldValues.count { it.contains(w, ignoreCase = true) } } + count
                 }
             }
 
@@ -61,58 +66,74 @@ sealed class ContactQuery {
     }
 
     companion object {
+        private val logger = KotlinLogging.logger {}
+
+        init {
+            logger.debug { "Supported contact fields in queries: ${ComplexQuery.fieldMap.keys}" }
+        }
+
         @JvmStatic fun parse(query: String): ContactQuery {
-            if (query.isBlank()) return EmptyQuery
+            val theQuery = query.trim().let { str ->
+                str.takeIf { it.count('"'::equals) % 2 == 0 } ?: str.lastIndexOf('"').let { i -> str.removeRange(i, i + 1) }
+            }
 
-            val words = mutableListOf<String>()
-            val fields = mutableMapOf<String, String>()
+            if (theQuery.replace(Regex("[\"\\s]"), "").isEmpty()) return EmptyQuery.also { logger.debug { "'$query' -> $it" } }
 
-            val buffer = mutableListOf<Char>()
+            val words = mutableSetOf<String>()
+            val fields = mutableMapOf<String, MutableSet<String>>()
 
-            var currentField = ""
+            lateinit var field: String
 
-            var state = State.NONE
+            var currState = State.NONE
             var fieldState = FieldState.NONE
 
-            val trim = query.trim()
-            trim.forEachIndexed { i, c ->
-                val prevState = state
-                when {
-                    c == ' ' -> if (prevState != QUOTED_LITERAL) state = NONE
-                    c == '"' -> {
-                        state = when (prevState) {
+            val buffer = StringBuilder()
+
+            theQuery.forEachIndexed { i, c ->
+                val prevState = currState
+                when (c) {
+                    ' ' -> {
+                        if (prevState != QUOTED_LITERAL) currState = NONE
+                        if (fieldState == NAME) fieldState = VALUE
+                    }
+                    '"' -> {
+                        currState = when (prevState) {
                             NONE, LITERAL -> QUOTED_LITERAL
                             QUOTED_LITERAL -> NONE
                         }
+                        if (fieldState == NAME) fieldState = VALUE
                     }
-                    c == ':' -> if (prevState == LITERAL) fieldState = NAME
-                    c.isLetterOrDigit() -> {
-                        if (prevState == NONE) state = LITERAL
-                        else if (fieldState == NAME) fieldState = VALUE
+                    ':' -> if (prevState == LITERAL) fieldState = NAME
+                    else -> {
+                        when {
+                            prevState == NONE -> currState = LITERAL
+                            fieldState == NAME -> fieldState = VALUE
+                        }
                     }
                 }
 
-                if ((state == LITERAL || (state == prevState && prevState == QUOTED_LITERAL)) && fieldState != NAME) buffer += c
+                if ((currState == LITERAL || (currState == prevState && prevState == QUOTED_LITERAL)) && fieldState != NAME) {
+                    buffer.append(c)
+                }
 
-                if (state == LITERAL && fieldState == NAME) {
-                    currentField = String(buffer.toCharArray())
+                if (currState == LITERAL && fieldState == NAME) {
+                    field = buffer.toString()
                     buffer.clear()
                 }
 
-                if (prevState == LITERAL && (state != LITERAL || i == trim.lastIndex) && fieldState == VALUE) {
-                    if (currentField in ComplexQuery.fieldMap.keys)
-                        fields[currentField] = String(buffer.toCharArray())
-                    currentField = ""
-                    buffer.clear()
-                }
-
-                if ((state == NONE && (prevState == LITERAL || prevState == QUOTED_LITERAL)) || i == trim.lastIndex) {
-                    if (buffer.isNotEmpty()) words.add(String(buffer.toCharArray()))
-                    buffer.clear()
+                if ((currState == NONE && currState != prevState) || (prevState == LITERAL && currState == QUOTED_LITERAL) || i == theQuery.lastIndex) {
+                    if (buffer.isNotEmpty()) {
+                        if (fieldState == VALUE) {
+                            field.takeIf { it in ComplexQuery.fieldMap.keys }?.let { fields.getOrPut(it) { mutableSetOf() }.add(buffer.toString()) }
+                        } else {
+                            words += buffer.toString()
+                        }
+                        buffer.clear()
+                    }
                 }
             }
 
-            return if (fields.isEmpty()) SimpleQuery(words) else ComplexQuery(words, fields)
+            return (if (fields.isEmpty()) SimpleQuery(words) else ComplexQuery(words, fields)).also { logger.debug { "'$query' -> $it" } }
         }
 
         private enum class State { NONE, LITERAL, QUOTED_LITERAL }
